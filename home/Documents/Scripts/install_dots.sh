650eedac18fead656639b0f0eb1fafdacc8e6c66
CauaStos <caua.oli.santos@gmail.com>
2025-04-28 09:16:13 -0300

    Updating dotfiles

--- home/Documents/Scripts/install_dots.sh
+++ home/Documents/Scripts/install_dots.sh
@@ -1,30 +1,28 @@
 #!/bin/bash
 
-#installing packages that are used by config. (Also installing AUR Helper of my preference, YAY)
+#installing packages that are used by config (and AUR helper).
 
-#yay install
+#paru install
 
-echo "Installing AUR Helper (YAY)"
+echo "Installing AUR Helper (Paru)"
 
-sudo pacman -S --needed --noconfirm git base-devel && git clone https://aur.archlinux.org/yay-bin.git && cd yay-bin && makepkg -si && cd .. #we 'cd ..' since we are inside of yay-bin
+sudo pacman -S --needed --noconfirm base-devel
+git clone https://aur.archlinux.org/paru.git
+cd paru
+makepkg -si && cd .. #we 'cd ..' since we are inside of paru
+
 
 #arch packages installation
 
 echo "Installing necessary packages (pacman)"
 
-sudo pacman -S --noconfirm hyprland unzip unrar sddm polkit-gnome swww hypridle nwg-look hyprlock zed thunar hyprpicker macchina zoxide starship mission-center pipewire pipewire-pulse pipewire-audio pipewire-alsa fish rsync file-roller qt5ct qt6ct gnome-keyring wtype wget
+sudo pacman -S --noconfirm hyprland unzip unrar sddm polkit-gnome swww hypridle nwg-look hyprlock zed thunar hyprpicker macchina zoxide starship mission-center pipewire pipewire-pulse pipewire-audio pipewire-alsa fish rsync file-roller qt5ct qt6ct gnome-keyring wtype wget fastfetch vlc
 
 #AUR packages installation
 
 echo "Installing necessary packages (AUR)"
 
-yay -S --noconfirm hyprsunset spicetify-cli zen-browser-bin spotify vesktop opentabletdriver hyprshot quicksand-font appimagelauncher matugen-bin aylurs-gtk-shell-git wezterm-git
-
-#Installing Papirus Icons
-echo "Installing Papirus icons (Local, not Global)"
-
-wget -qO- https://git.io/papirus-folders-install | env PREFIX=$HOME/.local sh
-wget -qO- https://git.io/papirus-icon-theme-install | env DESTDIR="$HOME/.local/share/icons" sh
+paru -S --noconfirm spicetify-cli zen-browser-bin spotify vesktop opentabletdriver hyprshot appimagelauncher matugen-bin wezterm-git
 
 #Installing fish
 
