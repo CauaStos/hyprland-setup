b6f730e32c1b99b0ab90364b8829ebd19801a229
CauaStos <caua.oli.santos@gmail.com>
2025-04-28 09:37:37 -0300

    Updating dotfiles

--- home/Documents/Scripts/install_dots.sh
+++ home/Documents/Scripts/install_dots.sh
@@ -1,51 +1,52 @@
 #!/bin/bash
 
-#installing packages that are used by config (and AUR helper).
+# AUR Helper and Packages installation.
 
-#paru install
-
+## Paru
 echo "Installing AUR Helper (Paru)"
 
 sudo pacman -S --needed --noconfirm base-devel
 git clone https://aur.archlinux.org/paru.git
 cd paru
-makepkg -si && cd .. #we 'cd ..' since we are inside of paru
+makepkg -si
+cd .. #we 'cd ..' since we are inside of paru/
 
 
-#arch packages installation
+## Packages installation
+echo "Installing necessary packages"
 
-echo "Installing necessary packages (pacman)"
-
+### Std
 sudo pacman -S --noconfirm hyprland unzip unrar sddm polkit-gnome swww hypridle nwg-look hyprlock zed thunar hyprpicker macchina zoxide starship mission-center pipewire pipewire-pulse pipewire-audio pipewire-alsa fish rsync file-roller qt5ct qt6ct gnome-keyring wtype wget fastfetch vlc
 
-#AUR packages installation
-
-echo "Installing necessary packages (AUR)"
-
+### AUR
 paru -S --noconfirm spicetify-cli zen-browser-bin spotify vesktop opentabletdriver hyprshot appimagelauncher matugen-bin wezterm-git
 
-#Installing fish
 
-echo "making fish the default shell."
+## Changing default shell
+echo "Changing the default shell to Fish."
 
 chsh -s /usr/bin/fish
 
-#Installing spicetify
 
-#spicetify
+## Enabling spicetify (TODO)
 
 
-#Copying folders to home
 
+# Moving files to home
 echo "Moving files to home. '~/'"
+
 rsync -av --checksum --mkpath ./Documents ./.config ./home/ ~/
 
-#Enabling Services
 
-echo "Enabling necessary services. (Just a precaution)"
 
-systemctl enable sddm
-systemctl enable NetworkManager
+# Enabling services
+echo "Enabling necessary services."
+
+systemctl enable NetworkManager --now
+
+
+
+# End
 
 echo "Done!"
 exit 0
